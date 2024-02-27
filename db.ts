48e81dd200589ddb5235f9f965e1878713d1a82c
Mahejabeen Naib (TA Digital) <sayed.mahejabeen@tadigital.com>
2024-02-27 17:22:43 +0530

    production db connection

--- db.ts
+++ db.ts
@@ -1,30 +1,39 @@
 import { loadEnvConfig } from "@next/env";
 import { Client, QueryResult } from "pg";
 
-export async function dbClient() : Promise<Client> {
+export async function dbClient(): Promise<Client> {
   const projectDir = process.cwd();
   loadEnvConfig(projectDir);
 
+  if (process.env.POSTGRES_URL) {
+    const client = new Client({
+      connectionString: process.env.POSTGRES_URL,
+    });
+
+    return client;
+  }
   const client = new Client({
     user: process.env.USER,
     password: process.env.PASSWORD,
     host: process.env.HOST,
     database: process.env.DB,
-    port: 5432
+    port: 5432,
   });
 
   return client;
 }
 
+export async function sql(
+  query: string,
+  values?: Array<any>
+): Promise<QueryResult<any>> {
+  const client = await dbClient();
 
-export async function sql(query:string , values?:Array<any>) : Promise<QueryResult<any>> {
-  const client = await dbClient()
+  await client.connect();
 
-  await client.connect()
+  const res = await client.query(query, values);
 
-  const res = await client.query(query , values)
-
-  await client.end()
+  await client.end();
 
   return res;
 }
